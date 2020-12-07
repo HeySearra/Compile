fc39e1929de0a233b56c35a97ab82a2a7d134356
HeySearra <48943939+HeySearra@users.noreply.github.com>
2020-12-08 01:10:01 +0800

    11

--- src/App.java
+++ src/App.java
@@ -20,33 +20,37 @@ import tokenizer.Tokenizer;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException, CompileError {
-        InputStream input;
-        PrintStream output;
-        DataOutputStream out;
-        System.out.println(args[1]);
-        String inputFileName=args[1],outputFileName=args[2];
-        input = new FileInputStream(new File(inputFileName));
-        output = new PrintStream(new FileOutputStream(new File(outputFileName)));
-        out=new DataOutputStream(new FileOutputStream(new File(outputFileName)));
-        Scanner scanner;
-        scanner = new Scanner(input);
-        StringIter iter = new StringIter(scanner);
-        Tokenizer tokenizer = tokenize(iter);
-        Analyser analyzer = new Analyser(tokenizer);
+        try {
+            InputStream input;
+            PrintStream output;
+            DataOutputStream out;
+            System.out.println(args[1]);
+            String inputFileName = args[1], outputFileName = args[2];
+            input = new FileInputStream(new File(inputFileName));
+            output = new PrintStream(new FileOutputStream(new File(outputFileName)));
+            out = new DataOutputStream(new FileOutputStream(new File(outputFileName)));
+            Scanner scanner;
+            scanner = new Scanner(input);
+            StringIter iter = new StringIter(scanner);
+            Tokenizer tokenizer = tokenize(iter);
+            Analyser analyzer = new Analyser(tokenizer);
 
-        System.out.println("\n------------------Analyser Start");
-        analyzer.analyse();
+            System.out.println("\n------------------Analyser Start");
+            analyzer.analyse();
 
-        System.out.println("\n----------------------------生成二进制");
-//            OutToBinary binary = new OutToBinary(Analyser.getGlobals(), Analyser.getStartFunction(), Analyser.getFunctionDefs());
-//
-//            DataOutputStream out = new DataOutputStream(new FileOutputStream(new File(args[1])));
-//            List<Byte> bytes = binary.generate();
-//            byte[] resultBytes = new byte[bytes.size()];
-//            for (int i = 0; i < bytes.size(); ++i) {
-//                resultBytes[i] = bytes.get(i);
-//            }
-//            out.write(resultBytes);
+            System.out.println("\n----------------------------生成二进制");
+            //            OutToBinary binary = new OutToBinary(Analyser.getGlobals(), Analyser.getStartFunction(), Analyser.getFunctionDefs());
+            //
+            //            DataOutputStream out = new DataOutputStream(new FileOutputStream(new File(args[1])));
+            //            List<Byte> bytes = binary.generate();
+            //            byte[] resultBytes = new byte[bytes.size()];
+            //            for (int i = 0; i < bytes.size(); ++i) {
+            //                resultBytes[i] = bytes.get(i);
+            //            }
+            //            out.write(resultBytes);
+        }catch (Exception e){
+            System.exit(-1);
+        }
     }
 
     private static Tokenizer tokenize(StringIter iter) {
